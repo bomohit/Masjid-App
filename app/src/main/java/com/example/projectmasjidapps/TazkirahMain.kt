8c5c95f04cb4a29820ee12cf4ffca350eb470220
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-08 15:51:44 +0800

    create layout and class for tazkirah main

---
+++ app/src/main/java/com/example/projectmasjidapps/TazkirahMain.kt
@@ -0,0 +1,12 @@
+package com.example.projectmasjidapps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TazkirahMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tazkirah_main)
+
+    }
+}
