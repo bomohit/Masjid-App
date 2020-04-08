1db7416ad735e633ba92f863d31c1e1e80cb9efb
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-09 05:06:49 +0800

    create layout and class for donate item

---
+++ app/src/main/java/com/example/projectmasjidapps/DonateItem.kt
@@ -0,0 +1,11 @@
+package com.example.projectmasjidapps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DonateItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.donate_item)
+    }
+}
