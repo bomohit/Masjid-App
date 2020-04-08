c974812598f22e1bb078652496a541510f2e360f
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-08 12:43:49 +0800

    create layout and class for LOGIN

---
+++ app/src/main/java/com/example/projectmasjidapps/LoginMain.kt
@@ -0,0 +1,11 @@
+package com.example.projectmasjidapps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_main)
+    }
+}
