26145d77f965b30aa6dfaad64590c0ac381acd05
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-09 06:07:01 +0800

    attach button donation from home to donation main

--- app/src/main/java/com/example/projectmasjidapps/HomeMain.kt
+++ app/src/main/java/com/example/projectmasjidapps/HomeMain.kt
@@ -1,11 +1,24 @@
 package com.example.projectmasjidapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.home_main.*
 
-class HomeMain : AppCompatActivity() {
+class HomeMain : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_main)
+
+        buttonDonation.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+
+        when (i) {
+            R.id.buttonDonation -> startActivity(Intent(this, DonationMain::class.java))
+        }
     }
 }
